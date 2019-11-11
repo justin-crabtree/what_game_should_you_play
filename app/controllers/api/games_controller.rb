e767af584c1e2bf0aaae40175d11ac11eaa36c48
crabtree <justin.kyle.crabtree@gmail.com>
2019-11-11 18:34:18 -0500

    changes game controller algorithms to fit with a minimum tag selected

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,58 +1,73 @@
 class Api::GamesController < ApplicationController
 
   def index
-    @games = Game.all
-    if params[:rating]
-      tag = Tag.find_by(name: params[:rating])
-      p "ratingtag======================#{tag}"
-      @games = tag.games
-    end
+    @games = Game.all.to_a
+    
     if params[:experienceLevel]
       tag = Tag.find_by(name: params[:experienceLevel])
-      p "experienceleveltag======================#{tag}"
+      
       @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
+
     if params[:gameLength]
       tag = Tag.find_by(name: params[:gameLength])
-      p "gamelengthtag======================#{tag}"
+      
       @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
+
     if params[:replayability]
       tag = Tag.find_by(name: params[:replayability])
-      p "replayabilitytag======================#{tag}"
+      
       @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
+
     if params[:gameFocus]
       tag = Tag.find_by(name: params[:gameFocus])
-      p "gamefocustag======================#{tag}"
+      
       @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
+
     if params[:playerAmount]
       tag = Tag.find_by(name: params[:playerAmount])
-      p "playeramounttag======================#{tag}"
+      
       @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
-    if params[:checkedGenres]
-      params[:checkedGenres].each do |theme|
-        tag = Tag.find_by(name: theme)
-        p "genrestag======================#{tag}"
-        @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
-      end
-    end
+
     if params[:platforms]
-      params[:platforms].each do |theme|
-        tag = Tag.find_by(name: theme)
-        p "platformtag======================#{tag}"
-        @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
-      end
+      tag = Tag.find_by(name: params[:platforms])
+      
+      @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
     end
+
+    if params[:checkedGenres]
+      selected_games = []
+      params[:checkedGenres].each do |genre|
+        tag = Tag.find_by(name: genre)
+        selected_games << @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
+        selected_games = selected_games.flatten!.uniq
+      end
+      @games = selected_games
+    end
+
     if params[:checkedThemes]
       params[:checkedThemes].each do |theme|
         tag = Tag.find_by(name: theme)
-        p "themetag======================#{tag}"
-        @games = @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
+        selected_games << @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
+        selected_games = selected_games.flatten!.uniq
       end
+      @games = selected_games
     end
+
+    if params[:checkedRatings]
+      params[:checkedRatings].each do |rating|
+        tag = Tag.find_by(name: rating)
+        
+        selected_games << @games.select {|game| game.game_tags.where(tag_id: tag.id).count > 0 }
+        selected_games = selected_games.flatten!.uniq
+      end
+      @games = selected_games
+    end
+
     render 'index.json.jb'
   end
 
